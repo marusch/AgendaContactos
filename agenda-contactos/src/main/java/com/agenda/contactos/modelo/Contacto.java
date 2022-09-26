9932c1a549ce14293c790cf7d68ef64ffeede9dc
Maru Schuindt <maruschuindt07@gmail.com>
2022-09-26 19:08:54 -0300

    Finalizado

--- agenda-contactos/src/main/java/com/agenda/contactos/modelo/Contacto.java
+++ agenda-contactos/src/main/java/com/agenda/contactos/modelo/Contacto.java
@@ -2,7 +2,6 @@ package com.agenda.contactos.modelo;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
